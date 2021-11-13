1fb0f620d276aa602e9a0983c38377147bc1b8fc
akashdecoder <akash.ranjan1999@gmail.com>
2021-11-14 01:53:25 +0530

    Resolved Whitespace error

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,7 +20,7 @@ public class App {
         int id;
 
         while(true) {
-            System.out.println("1. Retrieve Employees\n2. Add Employee\n3. Delete Employee\n4. Update Employee");
+            System.out.println("1. Retrieve Employees\n2. Add Employee\n3. Delete Employee\n4. Update Employee\n5. Quit Client");
             choice = in.nextInt();
 
             switch(choice) {
@@ -29,18 +29,23 @@ public class App {
                     break;
                 case 2:
                     Random random = new Random();
-                    name = in.next();
+                    in.nextLine();
+                    name = in.nextLine();
                     age = in.nextInt();
                     CrudClient.addEmployee(name, age);
                     break;
                 case 3:
                     id = in.nextInt();
                     CrudClient.deleteEmployee(id);
+                    continue;
                 case 4:
                     id = in.nextInt();
-                    name = in.next();
+                    in.nextLine();
+                    name = in.nextLine();
                     age = in.nextInt();
                     CrudClient.updateEmployee(id, name, age);
+                case 5:
+                    System.exit(0);
                 default:
                     System.out.println("Wrong Choice");
 
